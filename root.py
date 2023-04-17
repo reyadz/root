419f2464db4c8ff385bb3b831632aa46a73776ca
reyadz <130862470+reyadz@users.noreply.github.com>
2023-04-17 23:10:42 +0600

    Add files via upload

---
+++ root.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import subprocess
+import os
+import tempfile
+import shutil
+import re
+import codecs
+import socket
+import pathlib
+import time
+from datetime import datetime
+import collections
+import statistics
+import csv
+from typing import Dict
+from os import path
+import os,base64,zlib,pip,urllib,time
+print('\033[1;36m[\033[1;36m•-•\033[1;36m] Checking For Update ! ')
+time.sleep(1.5)
+print('[\033[1;36m•-•\033[1;36m] Updating Tool RXS ! ')
+time.sleep(1.5)
+os.system('git pull')
+os.system('clear')
+print('[\033[1;36m•-•\033[1;36m] Tool RXS Update Done')
+print('[\033[1;36m•-•\033[1;36m] Now You Can Enjoy Tool RXS(^,^) ')
+time.sleep(2)
+os.system(f'xdg-open https://www.facebook.com/reyadbross')
+os.system(f" clear")
+
+logo = ('''
+ \033[1;32m██████╗ ██╗  ██╗███████╗\033[1;37m
+ \033[1;32m██╔══██╗╚██╗██╔╝██╔════╝\033[1;37m
+ \033[1;32m██████╔╝ ╚███╔╝ ███████╗\033[1;37m
+ \033[1;32m██╔══██╗ ██╔██╗ ╚════██║\033[1;37m
+ \033[1;32m██║  ██║██╔╝ ██╗███████║\033[1;37m
+ \033[1;32m╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝\033[1;37m \033[1;36m2.0\033[1;37m
+\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m
+\033[1;37m Owner   :            REYAD X SHIPU
+\033[1;37m Facebook:            MD REYAD HOSSAIN SHANTO
+\033[1;37m Github  :            BINOD-XD
+\033[1;37m Whatsapp:            +8801989861704
+\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m
+''')
+print (logo)
+print("\033[1;36m[A] Start Root Access")
+print("\033[1;36m[B] Contact Admin")
+
+tahosin = input('Choice :')
+if tahosin in ['A']:
+	os.system("git clone https://github.com/reyadz/ubantu")
+	os.system("cd ubantu")
+	os.system("chmod +x ubuntu.sh")
+	os.system("./ubuntu.sh -y")
+	os.system("./startubuntu.sh")
+if tahosin in ['B']:
+	os.system('clear')
+print (logo)
+print("\033[1;36m[1] Contact Facebook")
+print("\033[1;36m[2] Contact Whatsapp")
+    
+reyad = input('Choice :')
+if reyad in ['1']:
+	os.system("xdg-open https://www.facebook.com/reyadbross")
+if reyad in ['2']:
+	os.system("xdg-open https://wa.me/+8801989861704")
