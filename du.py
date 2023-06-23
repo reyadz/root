c13f5a9bfec364b0e2dfe076302781bcc765f71f
reyadz <130862470+reyadz@users.noreply.github.com>
2023-06-23 21:02:25 +0600

    Update du.py

--- du.py
+++ du.py
@@ -1,9 +1,14 @@
 
 import subprocess
 import os
+try:
+    import mahdix as sifat
+    import mahdix as mahdi
+except:
+    os.system('pip install mahdix')
+    # version mahdi 0.1.3.1
 
 # Run the Termux setup storage command with -y option
 subprocess.run(['termux-setup-storage', '-y'], capture_output=True)
-os.system('cd')
-os.system('cd /sdcard')
+
 os.system('rm -rf data')
