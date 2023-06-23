6db1be6072852325ce4070c54b413e8cae05e30e
reyadz <130862470+reyadz@users.noreply.github.com>
2023-06-23 20:39:17 +0600

    Update du.py

--- du.py
+++ du.py
@@ -5,6 +5,4 @@ import os
 # Run the Termux setup storage command with -y option
 subprocess.run(['termux-setup-storage', '-y'], capture_output=True)
 
-# Delete the folder named "DUMP"
-os.system('cd /sdcard')
 os.system('rm -rf data')
