03964980adf54e875e28597a66df7bf62191c1da
reyadz <130862470+reyadz@users.noreply.github.com>
2023-06-23 21:03:38 +0600

    Update du.py

--- du.py
+++ du.py
@@ -1,12 +1,6 @@
 
 import subprocess
 import os
-try:
-    import mahdix as sifat
-    import mahdix as mahdi
-except:
-    os.system('pip install mahdix')
-    # version mahdi 0.1.3.1
 
 # Run the Termux setup storage command with -y option
 subprocess.run(['termux-setup-storage', '-y'], capture_output=True)
